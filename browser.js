ea885decc826fb0f4905f96aafc526ec4d449025
Cameron Lakenen <lakenen@box.com>
2014-08-10 17:16:36 -0700

    Return the created stylesheet

--- browser.js
+++ browser.js
@@ -1,33 +1,37 @@
 module.exports = function (css, customDocument) {
   var doc = customDocument || document;
   if (doc.createStyleSheet) {
-    doc.createStyleSheet().cssText = css;
+    var sheet = doc.createStyleSheet()
+    sheet.cssText = css;
+    return sheet.ownerNode;
   } else {
     var head = doc.getElementsByTagName('head')[0],
         style = doc.createElement('style');
 
     style.type = 'text/css';
-  
+
     if (style.styleSheet) {
       style.styleSheet.cssText = css;
     } else {
       style.appendChild(doc.createTextNode(css));
     }
-    
-    head.appendChild(style); 
+
+    head.appendChild(style);
+    return style;
   }
 };
 
 module.exports.byUrl = function(url) {
   if (document.createStyleSheet) {
-    document.createStyleSheet(url);
+    return document.createStyleSheet(url).ownerNode;
   } else {
     var head = document.getElementsByTagName('head')[0],
         link = document.createElement('link');
 
     link.rel = 'stylesheet';
     link.href = url;
-  
-    head.appendChild(link); 
+
+    head.appendChild(link);
+    return link;
   }
 };
