580da38b6b1ed9f4eb517e4bce34f6b7dc7449ed
Sergey Slipchenko <faergeek@gmail.com>
2015-11-07 09:36:53 +0300

    Let the user to decide what to process

--- index.js
+++ index.js
@@ -3,6 +3,18 @@
 var path = require('path')
 var through = require("through");
 
+var RegExpRE = /^\/(.*)\/(.*)$/;
+var cssRE = /\.css$/i;
+
+function stringToRegExp(str) {
+    var match = RegExpRE.exec(str);
+    if (!match) return;
+
+    var re = match[1];
+    var flags = match[2];
+    return new RegExp(re, flags);
+}
+
 module.exports = function (fileName, options) {
 
     options = options || {};
@@ -10,8 +22,22 @@ module.exports = function (fileName, options) {
         options['auto-inject'] = true;
     }
 
-    if (!/\.css$/i.test(fileName)) {
-        return through();
+    if (options.test) {
+        if (typeof(options.test) === 'string') {
+            options.test = stringToRegExp(options.test);
+        }
+    } else {
+        options.test = cssRE;
+    }
+
+    if (typeof options.test === 'function') {
+        if (!options.test(fileName)) {
+            return through();
+        }
+    } else if (options.test instanceof RegExp) {
+        if (!options.test.test(fileName)) {
+            return through();
+        }
     }
 
     var inputString = "";
