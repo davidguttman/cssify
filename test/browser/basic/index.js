040fd7916f8d92518494eff63abe9be07be3e1fd
Sergey Slipchenko <faergeek@gmail.com>
2015-12-05 09:53:54 +0300

    Simulate HMR for testing

--- test/browser/basic/index.js
+++ test/browser/basic/index.js
@@ -1,12 +1,50 @@
 'use strict'
 
-var test = require('tape-catch')
+var tapeCatch = require('tape-catch')
 var css = require('./style.css')
+var injectCss = require('../../..')
+
+var styleId = 'injection-testing'
+
+function test (desc, fn) {
+  tapeCatch(desc, function (t) {
+    try {
+      setTimeout(function () { fn(t) }, 0)
+    } catch (err) {
+      t.fail(err)
+    }
+  })
+}
 
 test('browser: basic usage', function (t) {
-  var position = window.getComputedStyle(document.body).position
-
   t.equal(css, 'body { position: absolute; }\n', 'styles exported')
-  t.equal(position, 'absolute', 'styles injected')
+
+  injectCss(css, undefined, styleId)
+  t.equal(
+    window.getComputedStyle(document.body).position,
+    'absolute',
+    'styles injected'
+  )
+
+  t.end()
+})
+
+test('browser: hot module replacement', function (t) {
+  injectCss('body { position: absolute; }\n', undefined, styleId)
+
+  t.equal(
+    window.getComputedStyle(document.body).position,
+    'absolute',
+    'first injection'
+  )
+
+  injectCss('', undefined, styleId)
+
+  t.equal(
+    window.getComputedStyle(document.body).position,
+    'static',
+    'second injection'
+  )
+
   t.end()
 })
